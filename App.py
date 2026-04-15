ed2345e6eb74f552f14ec4be044c06f1cf55bb78
kusumanjali16 <kusumanjalint15@gmail.com>
2026-04-15 11:16:25 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from groq import Groq
+
+st.set_page_config("SugnyanAI Context Generator",layout="wide")
+st.title("SugnyanAI-Context Generator")
+st.image("C:\Users\Windows\Pictures\Screenshots")
+
+client=Groq(api_key=st.secrets["GROQ_API_KEY"])
+product=st.text_input("product")
+audience.text_input("Audience")
+#BUTTOM TO Generate Content 
+if st.button("Generate Content"):
+  prompt=f"write marketing content for {product} targeting {audience}."
+  response = client.chat.completion.create(
+      model="llama-3.3-70b-versatile",
+      messages=[{"role":"user","content":prompt}]
+  )
+  st.session_state.text = response.choices[0].message.content
+  text =response.choices[0].message.content
+  st.write(text)
+
+if "text" in st.session_state:
+  content = st.text_area("Genearted Content", st.session_state.text,height=300)
+  st.download_button(
+            label="Download as TXT",
+            data=content,
+            file_name="marketing_copy.txt",
+            mime="text/plain"
+  )
+else:
+  st.info("Generate content first")
